884c2daea2021782c1e3a050cf664a27f68a3132
mike <mikesapienzaaudio@gmail.com>
2020-10-07 10:51:33 -0500

    finished

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+
+    attr_accessor :name, :weight, :height
+    @@pirates = []
+    
+    def initialize(info)
+        @name = info[:name]
+        @weight = info[:weight]
+        @height = info[:height]
+        @@pirates.push(self)
+    end
+    
+    def self.all
+        @@pirates
+    end
+    
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,22 @@
 class Ship
+
+    attr_accessor :name, :type, :booty
+    @@ships = []
+    
+    def initialize(info)
+        @name = info[:name]
+        @type = info[:type]
+        @booty = info[:booty]
+        @@ships.push(self)
+    end
+    
+    def self.all
+        @@ships
+    end
+
+    def self.clear
+        @@ships = []
+    end
+    
+
 end
